39ba27dc5e9af8fec96dcfd80a2eeba6103405d2
NANDITA PAUL <139105741+nandita618@users.noreply.github.com>
2024-08-24 16:44:16 +0530

    Add files via upload

---
+++ crypto_price_prediction.py
@@ -0,0 +1,726 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# First we will import the necessary Library 
+
+import os
+import pandas as pd
+import numpy as np
+import math
+import datetime as dt
+
+# For Evalution we will use these library
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, explained_variance_score, r2_score 
+from sklearn.metrics import mean_poisson_deviance, mean_gamma_deviance, accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+
+# For model building we will use these library
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import LSTM
+
+
+# For PLotting we will use these library
+
+import matplotlib.pyplot as plt
+from itertools import cycle
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+
+# In[2]:
+
+
+# Load our dataset 
+# Note it should be in same dir
+
+
+maindf=pd.read_csv('BTC-USD (1).csv')
+
+
+# In[3]:
+
+
+print('Total number of days present in the dataset: ',maindf.shape[0])
+print('Total number of fields present in the dataset: ',maindf.shape[1])
+
+
+# In[4]:
+
+
+maindf.shape
+
+
+# In[6]:
+
+
+maindf.head()
+
+
+# In[7]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# In[8]:
+
+
+maindf.info()
+
+
+# In[9]:
+
+
+maindf.describe()
+
+
+# In[10]:
+
+
+print('Null Values:',maindf.isnull().values.sum())
+
+
+# In[11]:
+
+
+print('NA values:',maindf.isnull().values.any())
+
+
+# In[12]:
+
+
+maindf.shape
+
+
+# In[13]:
+
+
+# Printing the start date and End date of the dataset
+
+sd=maindf.iloc[0][0]
+ed=maindf.iloc[-1][0]
+
+
+print('Starting Date',sd)
+print('Ending Date',ed)
+
+
+# In[14]:
+
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2014 = maindf.loc[(maindf['Date'] >= '2014-09-17')
+                     & (maindf['Date'] < '2014-12-31')]
+
+y_2014.drop(y_2014[['Adj Close','Volume']],axis=1)
+
+
+# In[15]:
+
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2014 = maindf.loc[(maindf['Date'] >= '2014-09-17')
+                     & (maindf['Date'] < '2014-12-31')]
+
+y_2014.drop(y_2014[['Adj Close','Volume']],axis=1)
+
+
+# In[16]:
+
+
+monthvise= y_2014.groupby(y_2014['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+monthvise
+
+
+# In[17]:
+
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(
+    x=monthvise.index,
+    y=monthvise['Open'],
+    name='Bitcoin Open Price',
+    marker_color='crimson'
+))
+fig.add_trace(go.Bar(
+    x=monthvise.index,
+    y=monthvise['Close'],
+    name='Bitcoin Close Price',
+    marker_color='lightsalmon'
+))
+
+fig.update_layout(barmode='group', xaxis_tickangle=-45, 
+                  title='Monthwise comparision between Bitcoin open and close price')
+fig.show()
+
+
+# In[18]:
+
+
+y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_high = y_2014.groupby(maindf['Date'].dt.strftime('%B'))['High'].max()
+monthvise_high = monthvise_high.reindex(new_order, axis=0)
+
+monthvise_low = y_2014.groupby(y_2014['Date'].dt.strftime('%B'))['Low'].min()
+monthvise_low = monthvise_low.reindex(new_order, axis=0)
+
+fig = go.Figure()
+fig.add_trace(go.Bar(
+    x=monthvise_high.index,
+    y=monthvise_high,
+    name='Bitcoin high Price',
+    marker_color='rgb(0, 153, 204)'
+))
+fig.add_trace(go.Bar(
+    x=monthvise_low.index,
+    y=monthvise_low,
+    name='Bitcoin low Price',
+    marker_color='rgb(255, 128, 0)'
+))
+
+fig.update_layout(barmode='group', 
+                  title=' Monthwise High and Low Bitcoin price')
+fig.show()
+
+
+# In[19]:
+
+
+names = cycle(['Bitcoin Open Price','Bitcoin Close Price','Bitcoin High Price','Bitcoin Low Price'])
+
+fig = px.line(y_2014, x=y_2014.Date, y=[y_2014['Open'], y_2014['Close'], 
+                                          y_2014['High'], y_2014['Low']],
+             labels={'Date': 'Date','value':'Bitcoin value'})
+fig.update_layout(title_text='Bitcoin analysis chart', font_size=15, font_color='black',legend_title_text='Bitcoin Parameters')
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+
+fig.show()
+
+
+# In[20]:
+
+
+maindf['Date'] = pd.to_datetime(maindf['Date'], format='%Y-%m-%d')
+
+y_2015 = maindf.loc[(maindf['Date'] >= '2015-01-01')
+                     & (maindf['Date'] < '2016-01-01')]
+
+y_2015.drop(y_2015[['Adj Close','Volume']],axis=1)
+
+
+# In[21]:
+
+
+monthvise= y_2015.groupby(y_2015['Date'].dt.strftime('%B'))[['Open','Close']].mean()
+new_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+             'September', 'October', 'November', 'December']
+monthvise = monthvise.reindex(new_order, axis=0)
+monthvise
+
+
+# In[22]:
+
+
+# Lets First Take all the Close Price 
+closedf = maindf[['Date','Close']]
+print("Shape of close dataframe:", closedf.shape)
+
+
+# In[23]:
+
+
+fig = px.line(closedf, x=closedf.Date, y=closedf.Close,labels={'date':'Date','close':'Close Stock'})
+fig.update_traces(marker_line_width=2, opacity=0.8, marker_line_color='orange')
+fig.update_layout(title_text='Whole period of timeframe of Bitcoin close price 2014-2022', plot_bgcolor='white', 
+                  font_size=15, font_color='black')
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+
+# In[24]:
+
+
+closedf = closedf[closedf['Date'] > '2021-02-19']
+close_stock = closedf.copy()
+print("Total data for prediction: ",closedf.shape[0])
+
+
+# In[25]:
+
+
+closedf
+
+
+# In[26]:
+
+
+fig = px.line(closedf, x=closedf.Date, y=closedf.Close,labels={'date':'Date','close':'Close Stock'})
+fig.update_traces(marker_line_width=2, opacity=0.8, marker_line_color='orange')
+fig.update_layout(title_text='Considered period to predict Bitcoin close price', 
+                  plot_bgcolor='white', font_size=15, font_color='black')
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+
+# In[27]:
+
+
+# deleting date column and normalizing using MinMax Scaler
+
+del closedf['Date']
+scaler=MinMaxScaler(feature_range=(0,1))
+closedf=scaler.fit_transform(np.array(closedf).reshape(-1,1))
+print(closedf.shape)
+
+
+# In[28]:
+
+
+# we keep the training set as 60% and 40% testing set
+
+training_size=int(len(closedf)*0.60)
+test_size=len(closedf)-training_size
+train_data,test_data=closedf[0:training_size,:],closedf[training_size:len(closedf),:1]
+print("train_data: ", train_data.shape)
+print("test_data: ", test_data.shape)
+
+
+# In[29]:
+
+
+# convert an array of values into a dataset matrix
+
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-time_step-1):
+        a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100 
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
+
+# In[30]:
+
+
+time_step = 15
+X_train, y_train = create_dataset(train_data, time_step)
+X_test, y_test = create_dataset(test_data, time_step)
+
+print("X_train: ", X_train.shape)
+print("y_train: ", y_train.shape)
+print("X_test: ", X_test.shape)
+print("y_test", y_test.shape)
+
+
+# In[41]:
+
+
+# reshape input to be [samples, time steps, features] which is required for LSTM
+X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
+
+print("X_train: ", X_train.shape)
+print("X_test: ", X_test.shape)
+
+
+# In[42]:
+
+
+model=Sequential()
+
+model.add(LSTM(10,input_shape=(None,1),activation="relu"))
+
+model.add(Dense(1))
+
+model.compile(loss="mean_squared_error",optimizer="adam")
+
+
+# In[43]:
+
+
+history = model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=200,batch_size=32,verbose=1)
+
+
+# In[44]:
+
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(loss))
+
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend(loc=0)
+plt.figure()
+
+
+plt.show()
+
+
+# In[45]:
+
+
+### Lets Do the prediction and check performance metrics
+train_predict=model.predict(X_train)
+test_predict=model.predict(X_test)
+train_predict.shape, test_predict.shape
+
+
+# In[46]:
+
+
+# Transform back to original form
+
+train_predict = scaler.inverse_transform(train_predict)
+test_predict = scaler.inverse_transform(test_predict)
+original_ytrain = scaler.inverse_transform(y_train.reshape(-1,1)) 
+original_ytest = scaler.inverse_transform(y_test.reshape(-1,1)) 
+
+
+# In[47]:
+
+
+# Evaluation metrices RMSE and MAE
+print("Train data RMSE: ", math.sqrt(mean_squared_error(original_ytrain,train_predict)))
+print("Train data MSE: ", mean_squared_error(original_ytrain,train_predict))
+print("Train data MAE: ", mean_absolute_error(original_ytrain,train_predict))
+print("-------------------------------------------------------------------------------------")
+print("Test data RMSE: ", math.sqrt(mean_squared_error(original_ytest,test_predict)))
+print("Test data MSE: ", mean_squared_error(original_ytest,test_predict))
+print("Test data MAE: ", mean_absolute_error(original_ytest,test_predict))
+
+
+# In[48]:
+
+
+print("Train data explained variance regression score:", 
+      explained_variance_score(original_ytrain, train_predict))
+print("Test data explained variance regression score:", 
+      explained_variance_score(original_ytest, test_predict))
+
+
+# In[49]:
+
+
+print("Train data R2 score:", r2_score(original_ytrain, train_predict))
+print("Test data R2 score:", r2_score(original_ytest, test_predict))
+
+
+# In[50]:
+
+
+# shift train predictions for plotting
+
+look_back=time_step
+trainPredictPlot = np.empty_like(closedf)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
+print("Train predicted data: ", trainPredictPlot.shape)
+
+# shift test predictions for plotting
+testPredictPlot = np.empty_like(closedf)
+testPredictPlot[:, :] = np.nan
+testPredictPlot[len(train_predict)+(look_back*2)+1:len(closedf)-1, :] = test_predict
+print("Test predicted data: ", testPredictPlot.shape)
+
+names = cycle(['Original close price','Train predicted close price','Test predicted close price'])
+
+
+plotdf = pd.DataFrame({'date': close_stock['Date'],
+                       'original_close': close_stock['Close'],
+                      'train_predicted_close': trainPredictPlot.reshape(1,-1)[0].tolist(),
+                      'test_predicted_close': testPredictPlot.reshape(1,-1)[0].tolist()})
+
+fig = px.line(plotdf,x=plotdf['date'], y=[plotdf['original_close'],plotdf['train_predicted_close'],
+                                          plotdf['test_predicted_close']],
+              labels={'value':'Stock price','date': 'Date'})
+fig.update_layout(title_text='Comparision between original close price vs predicted close price',
+                  plot_bgcolor='white', font_size=15, font_color='black', legend_title_text='Close Price')
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+
+# In[52]:
+
+
+x_input=test_data[len(test_data)-time_step:].reshape(1,-1)
+temp_input=list(x_input)
+temp_input=temp_input[0].tolist()
+
+from numpy import array
+
+lst_output=[]
+n_steps=time_step
+i=0
+pred_days = 30
+while(i<pred_days):
+    
+    if(len(temp_input)>time_step):
+        
+        x_input=np.array(temp_input[1:])
+        #print("{} day input {}".format(i,x_input))
+        x_input = x_input.reshape(1,-1)
+        x_input = x_input.reshape((1, n_steps, 1))
+        
+        yhat = model.predict(x_input, verbose=0)
+        #print("{} day output {}".format(i,yhat))
+        temp_input.extend(yhat[0].tolist())
+        temp_input=temp_input[1:]
+        #print(temp_input)
+       
+        lst_output.extend(yhat.tolist())
+        i=i+1
+    else:
+        
+        x_input = x_input.reshape((1, n_steps,1))
+0        yhat = model.predict(x_input, verbose=0)
+        temp_input.extend(yhat[0].tolist())
+        
+        lst_output.extend(yhat.tolist())
+        i=i+1
+print("Output of predicted next days: ", len(lst_output))
+               
+
+
+# In[53]:
+
+
+last_days=np.arange(1,time_step+1)
+day_pred=np.arange(time_step+1,time_step+pred_days+1)
+print(last_days)
+print(day_pred)
+
+
+# In[54]:
+
+
+temp_mat = np.empty((len(last_days)+pred_days+1,1))
+temp_mat[:] = np.nan
+temp_mat = temp_mat.reshape(1,-1).tolist()[0]
+
+last_original_days_value = temp_mat
+next_predicted_days_value = temp_mat
+
+last_original_days_value[0:time_step+1] = scaler.inverse_transform(closedf[len(closedf)-time_step:]).reshape(1,-1).tolist()[0]
+next_predicted_days_value[time_step+1:] = scaler.inverse_transform(np.array(lst_output).reshape(-1,1)).reshape(1,-1).tolist()[0]
+
+new_pred_plot = pd.DataFrame({
+    'last_original_days_value':last_original_days_value,
+    'next_predicted_days_value':next_predicted_days_value
+})
+
+names = cycle(['Last 15 days close price','Predicted next 30 days close price'])
+
+fig = px.line(new_pred_plot,x=new_pred_plot.index, y=[new_pred_plot['last_original_days_value'],
+                                                      new_pred_plot['next_predicted_days_value']],
+              labels={'value': 'Stock price','index': 'Timestamp'})
+fig.update_layout(title_text='Compare last 15 days vs next 30 days',
+                  plot_bgcolor='white', font_size=15, font_color='black',legend_title_text='Close Price')
+
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+
+# In[55]:
+
+
+lstmdf=closedf.tolist()
+lstmdf.extend((np.array(lst_output).reshape(-1,1)).tolist())
+lstmdf=scaler.inverse_transform(lstmdf).reshape(1,-1).tolist()[0]
+
+names = cycle(['Close price'])
+
+fig = px.line(lstmdf,labels={'value': 'Stock price','index': 'Timestamp'})
+fig.update_layout(title_text='Plotting whole closing stock price with prediction',
+                  plot_bgcolor='white', font_size=15, font_color='black',legend_title_text='Stock')
+
+fig.for_each_trace(lambda t:  t.update(name = next(names)))
+
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.show()
+
+
+# In[56]:
+
+
+import pickle
+
+
+# In[57]:
+
+
+# Create the directory if it does not exist
+os.makedirs("model", exist_ok=True)
+
+
+# In[59]:
+
+
+# Save the model to a file
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+print("Model saved successfully.")
+
+
+# In[60]:
+
+
+# Save the model using TensorFlow/Keras methods
+model.save('model.h5')
+
+
+# In[61]:
+
+
+from tensorflow.keras.models import load_model
+
+# Load the model
+model = load_model('model.h5')
+
+
+# In[63]:
+
+
+from tensorflow.keras.models import load_model
+
+# Load the model
+model = load_model('model.h5')
+
+
+# In[65]:
+
+
+import nbformat
+from nbconvert import PythonExporter
+
+# Load the notebook file
+with open('crypto_price_prediction.ipynb') as f:
+    notebook = nbformat.read(f, as_version=4)
+
+# Create a PythonExporter object
+exporter = PythonExporter()
+
+# Convert the notebook to a Python script
+script, _ = exporter.from_notebook_node(notebook)
+
+# Save the Python script to a file
+with open('crypto_price_prediction.py', 'w') as f:
+    f.write(script)
+
+print("Conversion complete: 'crypto_price_prediction.py' has been created.")
+
+
+# In[ ]:
+
+
+
+
